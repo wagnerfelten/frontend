063a48d778bc0b9904fdf4384401e1db3cdf03ce
Wagner Rodrigues <69311486+wagnerfelten@users.noreply.github.com>
2023-10-27 23:18:58 -0300

    Create home for users common

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,25 @@
+import { Container, CardContent, Content, CardsFood } from "./style";
+
+import comidas from "../../assets/img/comidas.svg";
 import Header from "../../Components/Header";
 
 function Home() {
   return (
     <>
       <Header />
-      <h1>hello word</h1>
+      <Container>
+        <CardContent>
+          <img src={comidas} alt="imagens de comidas" />
+          <Content>
+            <h2>Sabores inigualáveis</h2>
+            <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
+          </Content>
+        </CardContent>
+
+        <CardsFood>
+          
+        </CardsFood>
+      </Container>
     </>
   );
 }
---
+++ src/pages/home/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  margin-top: 54px;
+`;
+
+export const CardContent = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #192227;
+  height: 120px;
+
+  margin: 10px;
+  border-radius: 10px;
+
+  > img{
+    width: 400px;
+  }
+`;
+
+export const Content = styled.div`
+  > h2 {
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+
+  > p {
+    font-size: 12px;
+  }
+`;
+
+export const CardsFood = styled.section`
+
+
+`;
